6f7946252815a69f591016c637cb9285a339ea15
enjomitch <devnull@localhost>
2013-12-07 11:30:39 +0100

    Reverting to single-dimension optimisation

--- tests/BisectionOptiTest/main.cpp
+++ tests/BisectionOptiTest/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 #include <Math/BinSearchOpti.hpp>
 #include "BinSubjSphere.h"
 
@@ -9,15 +11,17 @@ int main()
 {
     double a = -10;
     double b = +10;
-    BinSearchOpti bsearch(a, b, 0.0001);
+    BinSearchOpti bsearch(a, b, 0.01);
 
     for (double shift = a-1; shift < b+1; shift += 0.8 )
     {
         BinSubjSphere sph(shift);
         Result<double> result = bsearch.Run(sph);
+        cout << setprecision(4) << fixed;
         cout << "Status: " << result.status;
         cout << ", x = " << shift;
         cout << ", xopt = " << result.value;
+        cout << ", xoptDif = " << fabs(result.value-shift);
         cout << ", y(xopt) = " << sph.UpdateGetValue(result.value);
         cout << endl;
     }
