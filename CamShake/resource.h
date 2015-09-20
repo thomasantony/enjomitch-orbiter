213cb5121c0b40437770270a96b74dfa25a3af9d
enjomitch <devnull@localhost>
2015-09-20 10:01:05 +0200

    Added CamShake

---
+++ CamShake/resource.h
@@ -0,0 +1,87 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by CamShake.rc
+//
+#define IDD_INTERFACE                   101
+#define IDD_OUTPUT                      102
+#define IDC_VESSELLIST                  1000
+#define IDC_FOCUS                       1001
+#define IDC_MAIN_SLIDER                 1003
+#define IDC_RETRO_SLIDER                1004
+#define IDC_HOVER_SLIDER                1005
+#define IDC_MAIN_EDIT                   1006
+#define IDC_RETRO_EDIT                  1007
+#define IDC_HOVER_EDIT                  1008
+#define IDC_RCS_YAWLEFT                 1011
+#define IDC_RCS_YAWRIGHT                1012
+#define IDC_RCS_PITCHUP                 1013
+#define IDC_RCS_PITCHDOWN               1014
+#define IDC_RCS_BANKLEFT                1015
+#define IDC_RCS_BANKRIGHT               1016
+#define IDC_RCS_UP                      1017
+#define IDC_RCS_LEFT                    1018
+#define IDC_RCS_RIGHT                   1019
+#define IDC_RCS_DOWN                    1020
+#define IDC_RCS_FORWARD                 1021
+#define IDC_RCS_BACK                    1022
+#define IDC_KILLROT                     1024
+#define IDC_KILLROT2                    1025
+#define IDC_HLEVEL                      1026
+#define IDC_PROGRADE                    1027
+#define IDC_RETROGRADE                  1028
+#define IDC_NORMAL                      1029
+#define IDC_ANTINORMAL                  1030
+#define IDC_RETRO_GAUGE                 1036
+#define IDC_MAIN_GAUGE                  1037
+#define IDC_RCSLEVEL                    1038
+#define IDC_HOVER_GAUGE                 1039
+#define IDC_CAM_POS_X_P                 1042
+#define IDC_CAM_POS_Z_P                 1043
+#define IDC_CAM_POS_Z_M                 1044
+#define IDC_CAM_POS_X_M                 1045
+#define IDC_CAM_POS_Y_P                 1046
+#define IDC_CAM_POS_Y_M                 1047
+#define IDC_CAM_ROT_Y_P                 1048
+#define IDC_CAM_FOV                     1049
+#define IDC_LBL_FOV                     1050
+#define IDC_CAM_ROT_Y_M                 1051
+#define IDC_CAM_ROT_X_P                 1052
+#define IDC_CAM_ROT_X_M                 1053
+#define IDC_CAM_RESET                   1054
+#define IDC_CAM_PARAM_EDIT              1055
+#define IDC_CAM_SHOW_PARAM              1055
+#define IDC_CHK_THRUST_ENABLED          1057
+#define IDC_CHK_ATMO_ENABLED            1058
+#define IDC_EDIT_TH_FREQ1               1059
+#define IDC_EDIT_TH_FREQ2               1060
+#define IDC_EDIT_TH_AMP1                1061
+#define IDC_EDIT_TH_AMP2                1062
+#define IDC_BTN_OK                      1063
+#define IDC_BTN_CANCEL                  1064
+#define IDC_BTN_TEST_TH                 1065
+#define IDC_EDIT_AT_FREQ1               1066
+#define IDC_EDIT_AT_FREQ2               1067
+#define IDC_EDIT_AT_AMP1                1068
+#define IDC_BTN_TEST_AT                 1069
+#define IDC_CHK_GROUND_ENABLED          1070
+#define IDC_EDIT_AT_AMP2                1071
+#define IDC_EDIT_GD_FREQ1               1072
+#define IDC_EDIT_GD_FREQ2               1073
+#define IDC_EDIT_GD_AMP1                1074
+#define IDC_BTN_TEST_GD                 1075
+#define IDC_EDIT_GD_AMP2                1076
+#define IDC_EDIT_GD_MAXSPD              1077
+#define IDC_STATIC_CLASSNAME            1078
+#define IDC_STATIC_MAXTHRUST            1079
+#define IDC_CHK_NOSTAGING               1080
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1081
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
